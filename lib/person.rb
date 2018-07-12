1abccfea244b63e44172c611e542c7ea323cda56
isabellademetz <isabella21136@gmail.com>
2018-07-12 22:42:38 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,14 +8,18 @@ class Person
   def name
     @name = name
 end
+
     def name= "Beyonce"
       @name = new_name
     end
+    
     def job
       @job = "Singer"
     end
+    
     def job=(new_job)
       @job = new_job
     end
+    
   end
     
