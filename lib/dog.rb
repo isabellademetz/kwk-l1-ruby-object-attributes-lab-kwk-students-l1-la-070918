7763fdb981d662df75d787c89c263b1992b03fd6
isabellademetz <isabella21136@gmail.com>
2018-07-12 22:31:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,13 +7,13 @@ class Dog
   def name
     @name = "Fido"
   end
-  def name = (new_name)
+  def name=(new_name)
     @name = new_name
   end
   def breed
     @breed = "Beagle"
 end
-def breed = (new_breed)
+def breed=(new_breed)
   @breed = new_breed
 end
   
